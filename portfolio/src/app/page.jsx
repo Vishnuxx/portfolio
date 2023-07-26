31fafabfabd6098671800faa92ce360e7ac8abc9
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-26 17:05:28 +0530

    Create: Hero Section

--- portfolio/src/app/page.jsx
+++ portfolio/src/app/page.jsx
@@ -4,6 +4,15 @@ export default function Home() {
   return (
     <main className="h-fit">
       <div className="bg-gradient-to-r from-rose-500 to-indigo-600 z-50 fixed bottom-0 left-0 right-0 h-[3px] origin-[0%]  " />
+      {/* Hero section */}
+      <section className="w-[full] h-screen flex flex-col overflow-x-hidden  justify-center items-center ">
+        <div className="animate-intro will-change-transform font-bold lg:text-[8rem] md:text-[5rem] sm:text-[4rem] text-[3rem] bg-gradient-to-r from-rose-500 to-indigo-600 bg-clip-text text-transparent ">
+          Vishnuverse
+        </div>
+        <p className=" animate-intro will-change-transform lg:text-[1.2rem] md:text-[1.1rem] sm:text-[1rem] text-[0.7rem]">
+          A World of a Crazy Hobbyst
+        </p>
+      </section>
     </main>
   );
 }
