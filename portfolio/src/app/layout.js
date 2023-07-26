8ea5448df6c020cbcc0f849a289e7e746174a5d9
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-26 20:55:40 +0530

    Add: meta tags

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -13,6 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-[100%]">
+      <meta charSet="UTF-8"></meta>
+      <meta
+        name="description"
+        content="Self Taught Developer Entrepreneur"
+      ></meta>
+      <meta
+        name="keywords"
+        content="Vishnu , Vishnuxx , Vishnu VS , Student , Entrepreneur"
+      ></meta>
+      <meta name="author" content="Vishnuxx"></meta>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
       <body
         className={`bg-black text-white h-screen flex flex-col box-border w-screen m-0 p-0 ${inter.className}`}
       >
