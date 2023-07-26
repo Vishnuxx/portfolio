39ec54d8f1f2ea8eb1781378b4a4d748eafa682e
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-27 00:32:14 +0530

    Update: Style text align in About section

--- portfolio/src/app/sections/About.jsx
+++ portfolio/src/app/sections/About.jsx
@@ -3,15 +3,15 @@ export default function About() {
     <div className="relative isolate overflow-hidden px- py-9 sm:py-32 lg:overflow-visible lg:px-0">
       <div className=" mx-auto grid  max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-          <div className="lg:pr-4">
-            <div className="lg:max-w-lg">
-              <p className="text-base font-semibold leading-7 text-indigo-600">
+          <div className="lg:pr-4 text-justify hyphens-auto">
+            <div className="lg:max-w-lg flex flex-col items-center ">
+              {/* <p className="text-base font-semibold leading-7 text-indigo-600 ">
                 Showcase your tech projects
-              </p>
-              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">
-                More About Me
+              </p> */}
+              <h1 className="mt-2 mb-6  text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">
+                About Me
               </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
+              <p className="mt-6 text-lg leading-8 text-gray-400">
                 Hey there, fellow tech geeks! Vishnu here! I'm just an ordinary
                 guy with an extraordinary love for all things . They call me a
                 budding entrepreneur, but honestly, I'm just a passionate soul
@@ -21,14 +21,14 @@ export default function About() {
             </div>
           </div>
         </div>
-        <div className=" -mt-12 p-12 lg:sticky flex justify-center max-w-[500px] items-center lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+        <div className=" -mt-12 p-12 lg:sticky w-full flex justify-center  items-center lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <img
             className="w-full max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10  object-contain"
             src="Vishnuxx-sm.png"
             alt="Portfolio Screenshot"
           />
         </div>
-        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 text-justify hyphens-auto">
           <div className="lg:pr-4">
             <div className="max-w-xl text-base leading-7 text-gray-400 lg:max-w-lg">
               <p>
