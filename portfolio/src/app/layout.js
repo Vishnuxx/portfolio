49450c51c9427fb084397f5e10feca4a9de8d604
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-26 16:52:19 +0530

    Implement: Navbar component

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import Navbar from "../components/NavBar";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,10 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-[100%]">
-     
       <body
         className={`bg-black text-white h-screen flex flex-col box-border w-screen m-0 p-0 ${inter.className}`}
       >
+        <Navbar></Navbar>
         <main className="h-fit w-full flex flex-col">{children}</main>
         <Footer></Footer>
       </body>
