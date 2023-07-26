55526dd34096db587ff89d2b449564dd158a1925
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-26 17:01:34 +0530

    Create: Bottom Gradient Design

--- portfolio/src/app/page.jsx
+++ portfolio/src/app/page.jsx
@@ -3,6 +3,7 @@ export default function Home() {
  
   return (
     <main className="h-fit">
+      <div className="bg-gradient-to-r from-rose-500 to-indigo-600 z-50 fixed bottom-0 left-0 right-0 h-[3px] origin-[0%]  " />
     </main>
   );
 }
