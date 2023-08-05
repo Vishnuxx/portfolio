c6b715edf1fc92e4854384c9e0c6c8a5083e68f8
vishnuxx <vishnuvs2002it@gmail.com>
2023-08-06 03:26:21 +0530

    next config: disabled lint on build

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	extends: "next",
+	eslint: { ignoreDuringBuilds: true },
+	rules: {
+		"@next/next/no-img-element": "off",
+	},
+};
 
 module.exports = nextConfig
